e6a24e9cd6fda34b1836d9a799c256dded617b38
Usuario <Usuario@JOHNDAY>
2020-08-05 23:05:16 -0500

    agregado de suma

--- tarea1.js
+++ tarea1.js
@@ -1,3 +1,4 @@
 const sumatoria =[3,5,2,4,2,4,3,2,5,4,3,2,2];
 const resultado =sumatoria.reduce((sumatoria,Elemento)=>sumatoria+Elemento,0);
 console.log("La sumatoraria del vector es ", resultado);
+const suma=0;
